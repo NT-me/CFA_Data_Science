0ffb00d0e9bf2261c399a5f02430dd59cae96bed
Oleg <oleg.buklemishev>
2021-07-27 13:09:23 +0200

    normalisation de données (poids,taille,age)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # %% Import des modules
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # %% Récupérations de la donnée
 rawData = pd.read_csv("dataset/athlete_events.csv")
@@ -33,3 +35,54 @@ clearData = clearData.drop_duplicates()
 print("Taille post nettoyage :" + str(len(clearData)))
 print("Il y'a donc une perte de " + str(len(rawData) - len(clearData)))
 clearData.head()
+
+
+
+
+#%%%
+# Normalisation des données
+
+# %%
+
+plt.hist(clearData["Height"], bins=100, label=['Taille'])
+plt.legend(loc="upper right")
+plt.show()
+
+#normalisation de la taille 
+clearData["Height"]=((clearData["Height"]-clearData["Height"].min())/(clearData["Height"].max()-clearData["Height"].min()))*1
+
+plt.hist(clearData["Height"], bins=100, label=['Taille'])
+plt.legend(loc="upper right")
+plt.show()
+
+# %%
+
+
+
+plt.hist(clearData["Weight"], bins=100, label=['Poids'])
+plt.legend(loc="upper right")
+plt.show()
+
+#normalisation du poids 
+clearData["Weight"]=((clearData["Weight"]-clearData["Weight"].min())/(clearData["Weight"].max()-clearData["Weight"].min()))*1
+
+plt.hist(clearData["Weight"], bins=100, label=['Poids'])
+plt.legend(loc="upper right")
+plt.show()
+# %%
+
+
+
+
+plt.hist(clearData["Age"], bins=60, label=['Age'])
+plt.legend(loc="upper right")
+plt.show()
+
+#normalisation de l'age 
+clearData["Age"]=((clearData["Age"]-clearData["Age"].min())/(clearData["Age"].max()-clearData["Age"].min()))*1
+
+plt.hist(clearData["Age"], bins=60, label=['Age'])
+plt.legend(loc="upper right")
+plt.show()
+
+# %%
